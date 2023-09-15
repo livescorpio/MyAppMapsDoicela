2958a1d604fe10cdc3ebf19a2883c79d2da54892
unknown <you@example.com>
2023-09-15 18:10:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myappmapsdoicela/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.myappmapsdoicela;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener  {
+    EditText txtLatitud, txtLongitud;
+    GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtLatitud= findViewById(R.id.TxtLatitud);
+        txtLongitud= findViewById(R.id.TxtLongitud);
+        SupportMapFragment mapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(""+latLng.latitude);
+        txtLongitud.setText(""+latLng.longitude);
+
+
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(""+latLng.latitude);
+        txtLongitud.setText(""+latLng.longitude);
+
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap= googleMap;
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+
+        LatLng ecuador= new LatLng(-1.027284, -79.462979);
+        mMap.addMarker(new MarkerOptions().position(ecuador).title("Ecuador"));
+        mMap.moveCamera((CameraUpdateFactory.newLatLng(ecuador)));
+
+
+
+    }
+}
